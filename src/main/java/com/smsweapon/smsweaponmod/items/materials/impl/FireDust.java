43a2f4ecf0e69fe3970a30ac506257abd3edc851
阿金 <disciple_sneaker@163.com>
2018-08-25 11:11:59 +0800

    贴图更新 (#3)

--- src/main/java/com/smsweapon/smsweaponmod/items/materials/impl/FireDust.java
+++ src/main/java/com/smsweapon/smsweaponmod/items/materials/impl/FireDust.java
@@ -2,8 +2,11 @@ package com.smsweapon.smsweaponmod.items.materials.impl;
 
 import com.smsweapon.smsweaponmod.SmithsWeapon;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+import net.minecraftforge.common.MinecraftForge;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,5 +18,12 @@ public class FireDust extends Item{
         this.setUnlocalizedName("fireDust");
         this.setTextureName("smsweaponmod:fireDust");
         this.setCreativeTab(CreativeTabs.tabMaterials);
+    }
+
+    //添加物品描述
+    @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean flag) {
+        list.add(StatCollector.translateToLocal("item.fireDust.desc0"));
+        super.addInformation(itemStack,player,list,flag);
     }
 }
