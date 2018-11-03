215ff4c1135cd125ab010bfc7cf1f9d35a2d5f01
阿金 <disciple_sneaker@163.com>
2018-11-03 20:25:24 +0800

    更新编码规范

--- src/main/java/com/smsweapon/smsweaponmod/items/weapons/impl/FireSword.java
+++ src/main/java/com/smsweapon/smsweaponmod/items/weapons/impl/FireSword.java
@@ -2,40 +2,59 @@ package com.smsweapon.smsweaponmod.items.weapons.impl;
 
 import com.smsweapon.smsweaponmod.SmithsWeapon;
 import com.smsweapon.smsweaponmod.items.moulds.Mould;
-import com.smsweapon.smsweaponmod.items.moulds.impl.SwordMould;
 import com.smsweapon.smsweaponmod.items.weapons.Weapon;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-public class FireSword extends Item implements Weapon {
 
+/**
+ * 炎之剑·武器
+ *
+ * @author 0newing
+ * @version 1.0
+ */
+public class FireSword extends Item implements Weapon
+{
+
+    /**
+     * 需要的模具
+     */
     private Mould mould;
+
+    /**
+     * 需要的材料
+     */
     private ItemStack itemStack;
 
-    public FireSword() {
+    public FireSword()
+    {
         super();
-        mould = SmithsWeapon.swordMould;
-        itemStack = new ItemStack(SmithsWeapon.fireDust,1);
+        mould = SmithsWeapon.getSwordMould();
+        itemStack = new ItemStack(SmithsWeapon.getFireDust(), 1);
         this.setUnlocalizedName("fireSword");
         this.setTextureName("smsweaponmod:fireSword");
         this.setCreativeTab(CreativeTabs.tabCombat);
     }
 
-    public Mould getMould() {
+    public Mould getMould()
+    {
         return mould;
     }
 
-    public void setMould(Mould mould) {
+    public void setMould(Mould mould)
+    {
         this.mould = mould;
     }
 
-    public ItemStack getItemStack() {
+    public ItemStack getItemStack()
+    {
         return itemStack;
     }
 
     @Override
-    public Mould decompose() {
+    public Mould decompose()
+    {
         return mould;
     }
 }
