6cfa8be62499c6fc6d881c9d9beb5cef0e8a8588
阿金 <disciple_sneaker@163.com>
2018-11-04 23:12:41 +0800

    初始化Releases分支

--- src/main/java/com/smsweapon/smsweaponmod/items/moulds/Mould.java
+++
@@ -1,8 +0,0 @@
-package com.smsweapon.smsweaponmod.items.moulds;
-
-import com.smsweapon.smsweaponmod.items.weapons.Weapon;
-import net.minecraft.item.ItemStack;
-
-public interface Mould {
-    public abstract Weapon synthetic(ItemStack itemStack);
-}
--- src/main/java/com/smsweapon/smsweaponmod/items/weapons/Weapon.java
+++
@@ -1,7 +0,0 @@
-package com.smsweapon.smsweaponmod.items.weapons;
-
-import com.smsweapon.smsweaponmod.items.moulds.Mould;
-
-public interface Weapon {
-    public abstract Mould decompose();
-}
